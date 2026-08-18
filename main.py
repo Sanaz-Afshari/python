7216cb341529375562148cd18db20e523d7e2a76
Sanaz-Afshari <zsaafshari@gmail.com>
2026-08-18 12:56:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
 print("Hello,GitHub!")
-income1 = [1200, 1500, 980, 1750, 2100, 1600]
-income1 = [1200, 1500, 980, 1750, 2100, 1600]
-total=sum(income1)
-n=len(income1)
-average=total/n
-print(income1)
-print(f"avg={average:.2f}")
-highest=max(income1)
-lowest=min(income1)
-print(f"max={highest}")
-print(f"min={lowest}")
-count=0
-for income in income1:
-    if income>1500:
-        count=count+1
-print(f"monthes above 1500={count}")
