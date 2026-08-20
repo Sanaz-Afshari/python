1cce064768c82a09195f80d707beb387eb53ed46
Sanaz-Afshari <zsaafshari@gmail.com>
2026-08-20 12:10:09 +0330

    Track set1.py

--- set1.py
+++ set1.py
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+print("Hello,GitHub!")
+income1 = [1200, 1500, 980, 1750, 2100, 1600]
+>>>>>>> a5abee0 (Track set1.py)
 income1 = [1200, 1500, 980, 1750, 2100, 1600]
 total=sum(income1)
 n=len(income1)
@@ -12,4 +17,8 @@ count=0
 for income in income1:
     if income>1500:
         count=count+1
+<<<<<<< HEAD
 print(f"monthes above 1500={count}")
+=======
+print(f"monthes above 1500={count}")
+>>>>>>> a5abee0 (Track set1.py)
